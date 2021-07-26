bc03cb76d1f4a7c593ade4f5cec78892e7b5bc29
tysonkaufmann <kaufmann91@gmail.com>
2021-07-25 22:48:13 -0500

    Implement csv reading

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+import csv
 from fpgrowth_py import fpgrowth
 itemSetList = [['{1,40s}', '{2,M}', '{3,MB}', '{4,ICU}', '{5,comm}'],
                ['{1,40s}', '{2,F}', '{3,MB}', '{4,nonICU}', '{5,comm}'],
                ['{1,40s}', '{2,M}', '{3,MB}', '{4,NULL}', '{5,NULL}'],
                ['{1,40s}', '{2,M}', '{3,ON}', '{4,ICU}', '{5,comm}']]
 
-freqItemSet = fpgrowth(itemSetList, minSup=3)
+file = open("./Datasets/covid19-truncated.csv", "r")
+csvData = csv.reader(file)
+
+itemSetList = []
+categories = next(csvData)
+for row in csvData:
+    #Modify each item and append to new list with cateogry number
+    catList = []
+    for index,item in enumerate(row):
+        catList.append("{"+str(index)+","+str(item)+"}")
+    #print(row)
+    #print(catList)
+    #Append modified category list to item set list
+    itemSetList.append(catList)
+
+freqItemSet = fpgrowth(itemSetList, minSup=20)
 
 for item in freqItemSet:
     print(item)
 
-print(len(freqItemSet))
+#print(len(freqItemSet))
