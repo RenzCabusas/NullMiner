4581e578403270ab548dd522437821009eec1333
tysonkaufmann <kaufmann91@gmail.com>
2021-08-08 16:05:09 -0500

    Tidy output

--- main.py
+++ main.py
@@ -3,31 +3,49 @@ from fpgrowth_py import fpgrowth
 
 NULL_VALUE = '99'
 INPUT_FILE = './Datasets/covid19-truncated.csv'
+SUPPORT = 60
+SUPPORT_PERCENTAGE = True
 
 itemSetList = [['{1,40s}', '{2,M}', '{3,MB}', '{44,ICU}', '{5,comm}'],
                ['{1,40s}', '{2,F}', '{3,MB}', '{44,nonICU}', '{5,comm}'],
                ['{1,40s}', '{2,M}', '{3,MB}', '{44,NULL}', '{5,NULL}'],
                ['{1,40s}', '{2,M}', '{3,ON}', '{44,ICU}', '{5,comm}']]
 
-# file = open(INPUT_FILE, "r")
-# csvData = csv.reader(file)
+file = open(INPUT_FILE, "r")
+csvData = csv.reader(file)
 
-# itemSetList = []
-# categories = next(csvData)
+itemSetList = []
 
-# for row in csvData:
-#     #Modify each item and append to new list with cateogry identifier
-#     catList = []
-#     for index,item in enumerate(row):
-#         if str(item) == NULL_VALUE:
-#             item = 'NULL'
-#         catList.append("{"+str(categories[index])+","+str(item)+"}")
-#     itemSetList.append(catList)
+# First line of csv file contains category headers, store them in seperate variable for use later
+categories = next(csvData)
 
-#TODO Possibly calculate a percentage to generate minSup (total entries to list)
-freqItemSet = fpgrowth(itemSetList, minSup=3, isNullEntriesIncluded=True)
+# Row count for calculating support in percentage
+numRows = 0
 
+# Read in each row within the input file (csv) and parse into a list
+for row in csvData:
+    #Modify each item and append to new list with cateogry identifier
+    catList = []
+    for index,item in enumerate(row):
+        if str(item) == NULL_VALUE:
+            item = 'NULL'
+        catList.append("{"+str(categories[index])+","+str(item)+"}")
+    itemSetList.append(catList)
+    numRows += 1
+
+# Calculate a percentage to generate minSup if flag set in constant
+support = SUPPORT
+if SUPPORT_PERCENTAGE:
+    support = round((support / 100) * numRows)
+
+# Build and mine the trees
+freqItemSet = fpgrowth(itemSetList, minSup=support, isNullEntriesIncluded=True)
+
+# Print out each frequent itemset
 for item in freqItemSet:
     print(item)
 
-print(len(freqItemSet))
+print('---END OF FREQUENT ITEMS---')
+print('Number of rows input: ', numRows)
+print('Minimum support used: ', support)
+print('Number of frequent itemsets: ', len(freqItemSet))
